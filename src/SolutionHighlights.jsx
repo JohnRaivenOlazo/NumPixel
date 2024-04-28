8153a6e2a1f9979cc352f74a08d61a69a11f6db4
John Raiven Olazo <johnraivenolazo@gmail.com>
2024-04-29 03:08:57 +0800

    Added Preload to Header

--- src/SolutionHighlights.jsx
+++ src/SolutionHighlights.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Fade } from "react-awesome-reveal";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog, faChartLine, faPalette } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faChartLine, faPalette, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const SolutionHighlights = () => {
   return (
@@ -12,43 +12,59 @@ const SolutionHighlights = () => {
             Discover its Unique Solutions
           </h2>
         </Fade>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-          <Fade cascade damping={0.2}>
-            <div className="solution-card bg-purple-800 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center text-center transition duration-300 transform hover:scale-105">
-              <div className="solution-icon mb-4">
-                <FontAwesomeIcon icon={faCog} className="text-pink-400 text-4xl" />
-              </div>
-              <h3 className="text-xl font-semibold mb-4 text-white">
-                Streamlined Workflow
-              </h3>
-              <p className="text-pink-100">
-                Say goodbye to manual processes and tedious calculations, and hello to efficiency and productivity.
-              </p>
-            </div>
-            <div className="solution-card bg-purple-800 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center text-center transition duration-300 transform hover:scale-105">
-              <div className="solution-icon mb-4">
-                <FontAwesomeIcon icon={faChartLine} className="text-pink-400 text-4xl" />
-              </div>
-              <h3 className="text-xl font-semibold mb-4 text-white">
-              Enhanced Accuracy
-              </h3>
-              <p className="text-pink-100">
-              Achieve best accuracy with its advanced algorithm ensure to produce precise results every time.
-              </p>
-            </div>
-            <div className="solution-card bg-purple-800 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center text-center transition duration-300 transform hover:scale-105">
-              <div className="solution-icon mb-4">
-                <FontAwesomeIcon icon={faPalette} className="text-pink-400 text-4xl" />
-              </div>
-              <h3 className="text-xl font-semibold mb-4 text-white">
-                Flexible Customization
-              </h3>
-              <p className="text-pink-100">
-                With customizable options, you have the flexibility to adapt and customize to your specific requirements.
-              </p>
-            </div>
-          </Fade>
-        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8">
+  <Fade cascade damping={0.2}>
+    {/* Box 1 */}
+    <div className="solution-card bg-purple-800 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center text-center transition duration-300 transform hover:scale-105">
+      <div className="solution-icon mb-4">
+        <FontAwesomeIcon icon={faCog} className="text-pink-400 text-4xl" />
+      </div>
+      <h3 className="text-xl font-semibold mb-4 text-white">
+        Streamlined Workflow
+      </h3>
+      <p className="text-pink-100">
+        Say goodbye to manual processes and tedious calculations, and hello to efficiency and productivity.
+      </p>
+    </div>
+    {/* Box 2 */}
+    <div className="solution-card bg-purple-800 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center text-center transition duration-300 transform hover:scale-105">
+      <div className="solution-icon mb-4">
+        <FontAwesomeIcon icon={faChartLine} className="text-pink-400 text-4xl" />
+      </div>
+      <h3 className="text-xl font-semibold mb-4 text-white">
+        100% Enhanced Accuracy
+      </h3>
+      <p className="text-pink-100">
+        Achieve best accuracy with its advanced algorithm ensure to produce precise results every time.
+      </p>
+    </div>
+    {/* Box 3 */}
+    <div className="solution-card bg-purple-800 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center text-center transition duration-300 transform hover:scale-105">
+      <div className="solution-icon mb-4">
+        <FontAwesomeIcon icon={faPalette} className="text-pink-400 text-4xl" />
+      </div>
+      <h3 className="text-xl font-semibold mb-4 text-white">
+        Flexible Customization
+      </h3>
+      <p className="text-pink-100">
+        With customizable options, you have the flexibility to adapt and customize to your specific requirements.
+      </p>
+    </div>
+    {/* Box 4 */}
+    <div className="solution-card bg-purple-800 rounded-lg shadow-lg p-8 flex flex-col items-center justify-center text-center transition duration-300 transform hover:scale-105">
+      <div className="solution-icon mb-4">
+      <FontAwesomeIcon icon={faHeart} className="text-pink-400 text-4xl" />
+      </div>
+      <h3 className="text-xl font-semibold mb-4 text-white">
+        User Friendly
+      </h3>
+      <p className="text-pink-100">
+        built-in interface and helpful features  make the experience of using it user-friendly, just like your favorite app.
+      </p>
+    </div>
+  </Fade>
+</div>
+
           <div className="mt-12">
         <Fade>
             <p className="text-lg text-white max-w-3xl text-center mx-auto">
