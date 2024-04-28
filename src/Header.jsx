a5bdb15907a34ebe7c6f5a52a30fb0f3bebe51c2
John Raiven Olazo <johnraivenolazo@gmail.com>
2024-04-29 02:22:07 +0800

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -60,7 +60,7 @@ const Header = ({ setCalculatorActive }) => { // Receive setCalculatorActive as 
     <>
       <div id="header" className={`header z-50 relative`}>
         <div className="relative top-0 w-full h-screenplus flex flex-col justify-center items-start z-20">
-          <video autoPlay loop className="absolute top-0 h-full w-full object-cover z-0">
+          <video autoPlay loop muted playsinline className="absolute top-0 h-full w-full object-cover z-0">
               <source src={headerVideo} type="video/mp4"></source>
           </video>
           <Fade>
