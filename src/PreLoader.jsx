3c9f439890a1ed3c2a2b09ea96b620c366eda939
John Raiven Olazo <johnraivenolazo@gmail.com>
2024-05-19 04:55:28 +0800

    Update PreLoader.jsx

--- src/PreLoader.jsx
+++ src/PreLoader.jsx
@@ -1,34 +1,51 @@
-import "./assets/styles/css/index.css"
+import React, { useEffect, useState } from 'react';
+import "./assets/styles/css/index.css";
 
-const PreLoader = () => {
+const PreLoader = ({ onVideoLoad }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Update isLoading state when the video is loaded
+    if (onVideoLoad) {
+      setIsLoading(true);
+    }
+
+    // Add a delay before setting isLoading to false
+    const delayTimeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000); // Adjust the delay time as needed
+
+    // Clear the timeout when the component unmounts or when the video is loaded
+    return () => {
+      clearTimeout(delayTimeout);
+    };
+  }, [onVideoLoad]); // Trigger effect when onVideoLoad changes
 
   return (
-    <>
-    <div className={`loader-container`}>
-    <div className="page">
-	<div className="loadContainer">
-	<div className="loadInner">
-		<div className="loadCircle">
-			<div className="loadCircleInner"></div>
-		</div>
-		<div className="loadCircle">
-			<div className="loadCircleInner"></div>
-		</div>
-		<div className="loadCircle">
-			<div className="loadCircleInner"></div>
-		</div>
-		<div className="loadCircle">
-			<div className="loadCircleInner"></div>
-		</div>
-		<div className="loadCircle">
-			<div className="loadCircleInner"></div>
-		</div>
-	</div>
-</div>
-</div>
+    <div className={`loader-container ${isLoading ? '' : 'loading-hidden'}`}>
+      <div className="page">
+        <div className="loadContainer">
+          <div className="loadInner">
+            <div className="loadCircle">
+              <div className="loadCircleInner"></div>
+            </div>
+            <div className="loadCircle">
+              <div className="loadCircleInner"></div>
+            </div>
+            <div className="loadCircle">
+              <div className="loadCircleInner"></div>
+            </div>
+            <div className="loadCircle">
+              <div className="loadCircleInner"></div>
+            </div>
+            <div className="loadCircle">
+              <div className="loadCircleInner"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </>
-  )
-}
+  );
+};
 
-export default PreLoader
+export default PreLoader;
