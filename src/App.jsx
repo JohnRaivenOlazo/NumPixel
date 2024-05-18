81d70e2f9034058a9117a375b410c2b8ff17d530
John Raiven Olazo <johnraivenolazo@gmail.com>
2024-05-18 21:59:36 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import Header from './Header.jsx';
-import About from './About.jsx'
+import About from './About.jsx';
 import PreLoader from './PreLoader.jsx';
 import Window from './Window.jsx';
 import './assets/styles/css/index.css';
 import Solution from './Solution.jsx';
 import Footer from './Footer.jsx';
+import headerVideo from '../public/Header.mp4'; // Import video source
 
 const App = () => {
   const [calculatorActive, setCalculatorActive] = useState(false);
@@ -15,22 +16,30 @@ const App = () => {
   const [isShown, setIsShown] = useState(false);
 
   useEffect(() => {
-    const Loading = () => {
+    const video = document.createElement('video'); // Create video element
+    video.src = headerVideo;
+    video.preload = 'auto';
+
+    const handleVideoLoad = () => {
+      // Add delay after video is loaded
       setTimeout(() => {
         setIsLoading(false);
         setTimeout(() => {
           setIsShown(true);
         }, 0);
-      }, 3800);
+      }, 3800); // Adjust the delay time as needed
     };
 
-    Loading();
+    video.addEventListener('loadeddata', handleVideoLoad);
+
+    return () => {
+      // Clean up event listener
+      video.removeEventListener('loadeddata', handleVideoLoad);
+    };
   }, []);
 
   return isLoading ? (
-    <>
-      <PreLoader />
-    </>
+    <PreLoader />
   ) : (
     <>
       <div className={`main ${isShown ? 'showed' : ''} ${calculatorActive ? 'blurred' : ''}`}>
