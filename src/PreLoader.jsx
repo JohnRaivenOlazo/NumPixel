3153f1c5b39bca20c0db6eca736250b8276b5bb0
John Raiven Olazo <johnraivenolazo@gmail.com>
2024-05-19 12:43:38 +0800

    Update PreLoader.jsx

--- src/PreLoader.jsx
+++ src/PreLoader.jsx
@@ -1,24 +1,12 @@
+// PreLoader.jsx
+
 import React, { useEffect, useState } from 'react';
-import "./assets/styles/css/index.css";
-
-const PreLoader = ({ onVideoLoad }) => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // Add a delay before setting isLoading to false
-    const delayTimeout = setTimeout(() => {
-      setIsLoading(false);
-    }, 3300);
-
-    // Clear the timeout when the component unmounts or when the video is loaded
-    return () => {
-      clearTimeout(delayTimeout);
-    };
-  }, [onVideoLoad]); // Trigger effect when onVideoLoad changes
-
+import './App'
+import './assets/styles/css/index.css';
+const PreLoader = () => {
   return (
-    <div className={`loader-container ${isLoading ? '' : 'loading-hidden'}`}>
-      <div className="page">
+    <div className="loader-container">
+      <div className={`page loading-hidden`}>
         <div className="loadContainer">
           <div className="loadInner">
             <div className="loadCircle">
