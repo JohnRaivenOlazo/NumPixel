08d507ca020e5d684c0c49a708ca6e4669c2b220
John Raiven Olazo <johnraivenolazo@gmail.com>
2024-06-18 09:07:17 +0800

    Update index.js

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,4 +1,3 @@
-export { default as Fraction } from 'Fraction.jsx';
 export { validateInputs } from './Validation.jsx';
 export { Superscript } from './Superscript.jsx'
 export { default as Factorial } from './Factorial.jsx';
