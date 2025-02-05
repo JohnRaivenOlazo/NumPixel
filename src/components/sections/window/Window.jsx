6ee53fa6b82e26fa252a3c0e41634a21428087d6
John Raiven D. Olazo <137252774+JohnRaivenOlazo@users.noreply.github.com>
2025-02-05 11:30:19 +0000

    Refactor window styles for improved design; remove unused background and border properties

--- src/components/sections/window/Window.jsx
+++ src/components/sections/window/Window.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import '../../../assets/styles/css/window.css'
 import WindowContent from './WindowContent.jsx'
 import openWindow from '../../hooks/openWindow.jsx'
+import Logo from '/logo.png'; // Adjust the path to your logo image
 
 const Window = ({ calculatorActive, setCalculatorActive }) => {
   
@@ -33,10 +34,10 @@ const Window = ({ calculatorActive, setCalculatorActive }) => {
           <div className="close" /><div className="minimize" onClick={handleMinimizeClick} /><div className="maximize" onClick={handleMaximizeClick} />
         </div>
         <div className="overflow-hidden h-full text-center mr-16 font-sans leading-5 text-sm font-bold text-white">
-          NumPixel
+          <img src={Logo} alt="NumPixel Logo" className="h-full py-1 mx-auto" />
         </div>
       </div>
-      <div className="window overflow-x-hidden overflow-y-auto h-full">
+      <div className="window bg-[#1a1a1a] rounded-ee-md border border-[#333] overflow-x-hidden overflow-y-auto h-full">
         <WindowContent handleCalculatorClick={handleCalculatorClick} />
       </div>
     </div>
--- src/components/sections/window/WindowContent.jsx
+++ src/components/sections/window/WindowContent.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import { Fade } from 'react-awesome-reveal';
 import NewtonImage from '/Newton.png';
 import LagrangeImage from '/Lagrange.png';
+import StirlingImage from '/Stirling.png';
 import DividedDifference from '../../interpolation/NewtonDivided';
 import NewtonForward from '../../interpolation/NewtonForward';
 import NewtonBackward from '../../interpolation/NewtonBackward';
 import LagrangeInterpolation from '../../interpolation/LagrangeInterpolation';
-import Stirling from '/Stirling.png';
 import StirlingInterpolation from '../../interpolation/StirlingInterpolation';
 
 const WindowContent = () => {
@@ -34,87 +34,89 @@ const WindowContent = () => {
   };
 
   return (
-    <div className="min-h-screen flex flex-col justify-start items-center bg-gradient-to-b from-gray-800 to-gray-900">
+    <div className="flex flex-col justify-start items-center bg-gradient-to-b">
       <Fade cascade>
-        <div className="bg-gray-700 my-5 p-8 rounded-lg shadow-lg w-full border border-gray-600">
-            {selectedInterpolation === null && (
-                <Fade>
-                <div className="bg-gray-800 py-1 px-4 rounded-lg shadow-lg border border-gray-600 mb-8">
-                    <h2 className="text-2xl font-semibold text-center text-white uppercase drop-shadow-xl font-sans">
-                      Interpolation Methods
-                    </h2>
-                 </div>
-                  <button
-                    className="bg-gray-600 bg-gradient-to-t from-gray-700 hover:bg-gray-500 text-white py-4 rounded-lg flex flex-col justify-center items-center transform transition ease duration-300 hover:scale-105 focus:outline-none w-full mb-4"
-                    onClick={() => handleInterpolationSelection('Newton')}
-                  >
-                    <img src={NewtonImage} alt="Newton" className="h-24 mb-2 rounded-full" />
-                    <span className="text-xl font-semibold">Newton</span>
-                  </button>
-                  <button
-                    className="bg-gray-600 bg-gradient-to-t from-gray-700 hover:bg-gray-500 text-white py-4 rounded-lg flex flex-col justify-center items-center transform transition duration-300 hover:scale-105 focus:outline-none w-full mb-4"
-                    onClick={() => handleInterpolationSelection('Lagrange')}
-                  >
-                    <img src={LagrangeImage} alt="Lagrange" className="h-24 mb-2 rounded-full" />
-                    <span className="text-xl font-semibold">Lagrange</span>
-                  </button>
-                  <button
-                    className="bg-gray-600 bg-gradient-to-t from-gray-700 hover:bg-gray-500 text-white py-4 rounded-lg flex flex-col justify-center items-center transform transition duration-300 hover:scale-105 focus:outline-none w-full"
-                    onClick={() => handleInterpolationSelection('Stirling')}
-                  >
-                    <img src={Stirling} alt="Stirling" className="h-24 mb-2 rounded-full" />
-                    <span className="text-xl font-semibold">Stirling</span>
-                  </button>
-                </Fade>
-            )}
-            {selectedInterpolation === 'Stirling' && (
-              <StirlingInterpolation />
-            )}
-            {selectedInterpolation === 'Newton' && (
-              <Fade cascade>
-                <div className="bg-gray-800 py-1 px-4 rounded-lg shadow-lg border border-gray-600 mb-8">
-                    <h2 className="text-2xl font-semibold text-center text-white uppercase drop-shadow-xl font-sans">
-                      Newton Interpolation
-                    </h2>
-                 </div>
-                <div className="space-y-4 w-full">
-                  <button
-                    className="bg-gray-600 bg-gradient-to-t from-gray-700 hover:bg-gray-500 text-white font-semibold py-4 rounded-lg mt-10 flex justify-center items-center transform transition duration-200 hover:scale-105 hover:drop-shadow-xl focus:outline-none w-full"
-                    onClick={() => handleInterpolationSelection('ForwardDifference')}
-                  >
-                    Newton's Forward Difference Interpolation
-                  </button>
-                  <button
-                    className="bg-gray-600 bg-gradient-to-t from-gray-700 hover:bg-gray-500 text-white font-semibold py-4 rounded-lg flex justify-center items-center transform transition duration-200 hover:scale-105 hover:drop-shadow-xl focus:outline-none w-full"
-                    onClick={() => handleInterpolationSelection('BackwardDifference')}
-                  >
-                    Newton's Backward Difference Interpolation
-                  </button>
-                  <button
-                    className="bg-gray-600 bg-gradient-to-t from-gray-700 hover:bg-gray-500 text-white font-semibold py-4 rounded-lg flex justify-center items-center transform transition duration-200 hover:scale-105 hover:drop-shadow-xl focus:outline-none w-full"
-                    onClick={() => handleInterpolationSelection('DividedDifference')}
-                  >
-                    Newton's Divided Difference Interpolation
-                  </button>
-                </div>
-              </Fade>
-            )}
+        <div className="bg-gray-800 my-5 p-8 rounded-lg shadow-lg w-full border border-gray-700">
+          {selectedInterpolation === null && (
             <Fade>
-              <div className="overflow-x-auto">
-                {selectedInterpolation === 'ForwardDifference' && <NewtonForward />}
-                {selectedInterpolation === 'BackwardDifference' && <NewtonBackward />}
-                {selectedInterpolation === 'DividedDifference' && <DividedDifference />}
+              <div className="bg-gray-900 py-2 px-6 rounded-lg shadow-lg border border-gray-700 mb-8">
+                <h2 className="text-3xl font-semibold text-center text-white uppercase drop-shadow-xl font-sans">
+                  Interpolation Methods
+                </h2>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <button
+                  className="bg-gray-700 bg-gradient-to-t from-gray-800 hover:bg-gray-600 text-white py-6 rounded-lg flex flex-col justify-center items-center transform transition ease duration-300 hover:scale-105 focus:outline-none"
+                  onClick={() => handleInterpolationSelection('Newton')}
+                >
+                  <img src={NewtonImage} alt="Newton" className="h-24 mb-4 rounded-full shadow-lg" />
+                  <span className="text-xl font-semibold">Newton</span>
+                </button>
+                <button
+                  className="bg-gray-700 bg-gradient-to-t from-gray-800 hover:bg-gray-600 text-white py-6 rounded-lg flex flex-col justify-center items-center transform transition ease duration-300 hover:scale-105 focus:outline-none"
+                  onClick={() => handleInterpolationSelection('Lagrange')}
+                >
+                  <img src={LagrangeImage} alt="Lagrange" className="h-24 mb-4 rounded-full shadow-lg" />
+                  <span className="text-xl font-semibold">Lagrange</span>
+                </button>
+                <button
+                  className="bg-gray-700 bg-gradient-to-t from-gray-800 hover:bg-gray-600 text-white py-6 rounded-lg flex flex-col justify-center items-center transform transition ease duration-300 hover:scale-105 focus:outline-none"
+                  onClick={() => handleInterpolationSelection('Stirling')}
+                >
+                  <img src={StirlingImage} alt="Stirling" className="h-24 mb-4 rounded-full shadow-lg" />
+                  <span className="text-xl font-semibold">Stirling</span>
+                </button>
               </div>
             </Fade>
-            {selectedInterpolation === 'Lagrange' && (
-              <Fade>
-                <LagrangeInterpolation />
-              </Fade>
-            )}
+          )}
+          {selectedInterpolation === 'Stirling' && (
+            <StirlingInterpolation />
+          )}
+          {selectedInterpolation === 'Newton' && (
+            <Fade cascade>
+              <div className="bg-gray-900 py-2 px-6 rounded-lg shadow-lg border border-gray-700 mb-8">
+                <h2 className="text-3xl font-semibold text-center text-white uppercase drop-shadow-xl font-sans">
+                  Newton Interpolation
+                </h2>
+              </div>
+              <div className="space-y-4 w-full">
+                <button
+                  className="bg-gray-700 bg-gradient-to-t from-gray-800 hover:bg-gray-600 text-white font-semibold py-4 rounded-lg mt-10 flex justify-center items-center transform transition duration-200 hover:scale-105 hover:drop-shadow-xl focus:outline-none w-full"
+                  onClick={() => handleInterpolationSelection('ForwardDifference')}
+                >
+                  Newton's Forward Difference Interpolation
+                </button>
+                <button
+                  className="bg-gray-700 bg-gradient-to-t from-gray-800 hover:bg-gray-600 text-white font-semibold py-4 rounded-lg flex justify-center items-center transform transition duration-200 hover:scale-105 hover:drop-shadow-xl focus:outline-none w-full"
+                  onClick={() => handleInterpolationSelection('BackwardDifference')}
+                >
+                  Newton's Backward Difference Interpolation
+                </button>
+                <button
+                  className="bg-gray-700 bg-gradient-to-t from-gray-800 hover:bg-gray-600 text-white font-semibold py-4 rounded-lg flex justify-center items-center transform transition duration-200 hover:scale-105 hover:drop-shadow-xl focus:outline-none w-full"
+                  onClick={() => handleInterpolationSelection('DividedDifference')}
+                >
+                  Newton's Divided Difference Interpolation
+                </button>
+              </div>
+            </Fade>
+          )}
+          <Fade>
+            <div className="overflow-x-auto">
+              {selectedInterpolation === 'ForwardDifference' && <NewtonForward />}
+              {selectedInterpolation === 'BackwardDifference' && <NewtonBackward />}
+              {selectedInterpolation === 'DividedDifference' && <DividedDifference />}
+            </div>
+          </Fade>
+          {selectedInterpolation === 'Lagrange' && (
+            <Fade>
+              <LagrangeInterpolation />
+            </Fade>
+          )}
 
           {selectedInterpolation !== null && (
             <button
-              className="bg-gradient-to-b bg-gray-600 hover:bg-gray-500 border border-gray-600 text-white font-bold py-1 px-4 rounded-lg flex justify-center items-center transform transition duration-300 hover:scale-105 focus:outline-none mt-6"
+              className="bg-gradient-to-b bg-gray-700 hover:bg-gray-600 border border-gray-700 text-white font-bold py-2 px-6 rounded-lg flex justify-center items-center transform transition duration-300 hover:scale-105 focus:outline-none mt-6"
               onClick={handleBackButtonClick}
             >
               Back
