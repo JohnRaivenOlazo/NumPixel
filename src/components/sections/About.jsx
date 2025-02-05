cc8b981ca5638450078f8400faea2c2099d7f58f
John Raiven D. Olazo <137252774+JohnRaivenOlazo@users.noreply.github.com>
2025-02-05 10:34:40 +0000

    Refactor About and window styles for improved design; update scrollbar styles and remove unused imports

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -52,7 +52,7 @@ const About = () => {
       });
     };
     const observer = new IntersectionObserver(observerCallback);
-    
+
     items.forEach((item) => {
       observer.observe(item);
     });
@@ -127,85 +127,82 @@ const About = () => {
       <div id="about" className="features-section pt-24 pb-20 relative z-20 -mb-1">
         <div className="container mx-auto px-4">
           <Fade><h2 className="text-4xl lg:text-5xl font-bold mb-8 text-white text-center">About</h2></Fade>
-          
+
           <div className='aboutAuthor'>
-      <div className='aboutCont'>
-        <img
-          alt='Logo'
-          src={logo}
-        />
-        <p className="text-xl text-gray-200 font-bolder">
-        <p>
-  <span className="font-bold">NumPixel</span> is a calculator for numerical interpolation. <br />
-  It allows users to calculate different numerical methods with real time visuals and solutions. <br />
-</p>
-</p>
-        <div className='athrBtn'></div>
-      </div>
-      </div>
-    <div className="timeline pb-40">
-      <ul>
-        {timelineData.map((item, index) => (
-          <li key={index} className={index % 2 === 0 ? 'even' : 'odd'}>
-            <div className="text-xl">{item.content}</div>
-          </li>
-        ))}
-      </ul>
-    </div>
-
-        <Fade triggerOnce>
-          <div className="wheel-title mt-20 p-5 sm:m-2 pt-6 rounded-lg">
-            <Fade>
-              <p className="text-lg text-white max-w-2xl font-bolder text-center m-auto">
-                Explore a wide range of numerical techniques integrated into this platform, providing precision and flexibility in your mathematical computations.
+            <div className='aboutCont'>
+              <img
+                alt='Logo'
+                src={logo}
+              />
+              <p className="text-xl text-gray-200 font-bolder">
+                <p>
+                  <span className="font-bold">NumPixel</span> is a calculator for numerical interpolation. <br />
+                  It allows users to calculate different numerical methods with real time visuals and solutions. <br />
+                </p>
               </p>
-            </Fade>
+              <div className='athrBtn'></div>
+            </div>
+          </div>
+          <div className="timeline pb-40">
+            <ul>
+              {timelineData.map((item, index) => (
+                <li key={index} className={index % 2 === 0 ? 'even' : 'odd'}>
+                  <div className="text-xl">{item.content}</div>
+                </li>
+              ))}
+            </ul>
+          </div>
 
-            <Fade triggerOnce>
-              <div className="wheel flex flex-col md:flex-row gap-2 lg:gap-8 mt-8 items-start md:items-center md:justify-center">
-                <div className="md:w-1/2 flex justify-center items-center">
-                  <img
-                    src={Interpolation}
-                    alt="Interpolation Illustration"
-                    className="w-full rounded-lg shadow-lg transition-all duration-300 ease hover:-skew-y-1 hover:scale-105"
-                  />
-                </div>
-                <div
-                  className="md:w-1/2 wheel-container outline-none"
-                  ref={wheelContainerRef}
-                  onWheel={handleWheel}
-                  tabIndex="0"
-                >
-                  <div className="wheel">
-                    {currentMethods.map((methodIndex, i) => (
-                      <div
-                        key={i}
-                        className={`wheel-item ${i === 1 && "active"} ${
-                          transitioning && "transitioning"
-                        } ${
-                          transitioning &&
-                          transitionDirection === "next" &&
-                          i === 2 &&
-                          "transitionNext"
-                        } ${
-                          transitioning &&
-                          transitionDirection === "prev" &&
-                          i === 0 &&
-                          "transitionPrev"
-                        }`}
-                        onClick={
-                          i === 0 ? handlePrev : i === 2 ? handleNext : null
-                        }
-                      >
-                        {interpolationMethods[methodIndex]}
-                      </div>
-                    ))}
+          <Fade triggerOnce>
+            <div className="wheel-title mt-20 p-5 sm:m-2 pt-6 rounded-lg">
+              <Fade>
+                <p className="text-lg text-white max-w-2xl font-bolder text-center m-auto">
+                  Explore a wide range of numerical techniques integrated into this platform, providing precision and flexibility in your mathematical computations.
+                </p>
+              </Fade>
+
+              <Fade triggerOnce>
+                <div className="wheel flex flex-col md:flex-row gap-2 lg:gap-8 mt-8 items-start md:items-center md:justify-center">
+                  <div className="md:w-1/2 flex justify-center items-center">
+                    <img
+                      src={Interpolation}
+                      alt="Interpolation Illustration"
+                      className="w-full rounded-lg shadow-lg transition-all duration-300 ease hover:-skew-y-1 hover:scale-105"
+                    />
+                  </div>
+                  <div
+                    className="md:w-1/2 wheel-container outline-none"
+                    ref={wheelContainerRef}
+                    onWheel={handleWheel}
+                    tabIndex="0"
+                  >
+                    <div className="wheel">
+                      {currentMethods.map((methodIndex, i) => (
+                        <div
+                          key={i}
+                          className={`wheel-item ${i === 1 && "active"} ${transitioning && "transitioning"
+                            } ${transitioning &&
+                            transitionDirection === "next" &&
+                            i === 2 &&
+                            "transitionNext"
+                            } ${transitioning &&
+                            transitionDirection === "prev" &&
+                            i === 0 &&
+                            "transitionPrev"
+                            }`}
+                          onClick={
+                            i === 0 ? handlePrev : i === 2 ? handleNext : null
+                          }
+                        >
+                          {interpolationMethods[methodIndex]}
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
-              </div>
-            </Fade>
-          </div>
-        </Fade>
+              </Fade>
+            </div>
+          </Fade>
         </div>
       </div>
     </>
