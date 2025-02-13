1029643e59d8c083a11f40086ecb92a227e7924b
John Raiven D. Olazo <137252774+JohnRaivenOlazo@users.noreply.github.com>
2025-02-13 14:07:31 +0000

    Refactor footer hr style

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -34,7 +34,7 @@ const Footer = () => {
             </ul>
           </div>
         </div>
-        <hr className="border-purple-400 my-8" />
+        <hr className="border-purple-800 my-8" />
         <div className="flex justify-center">
           <a href="#header" className="text-sm text-gray-300 hover:text-white transition duration-300">Back to Top</a>
         </div>
